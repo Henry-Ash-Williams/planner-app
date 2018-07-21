08f0e8746cc9e0e93f24cc7355362822f738ceac
Henry Williams <henrywilliams95000@gmail.com>
2018-07-21 12:20:57 +0100

    Created Components Directory, Created Navbar Component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './css/css/index.css';
+
+class Navbar extends Component {
+    render() {
+        <div className="Navbar">
+            
+        </div>
+    }
+}
