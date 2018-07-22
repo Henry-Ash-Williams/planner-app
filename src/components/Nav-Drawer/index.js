db989296f8d8bf1cd83750efce50d05c6a82da0b
Henry Williams <henrywilliams95000@gmail.com>
2018-07-22 16:14:29 +0100

    Finished the nav-drawer toggle

--- src/components/Nav-Drawer/index.js
+++ src/components/Nav-Drawer/index.js
@@ -3,42 +3,37 @@ import './css/css/index.css';
  
 class NavDrawer extends Component {
     render() {
-        if (!this.props.hidden) {
-            return ( 
-                <div className="Nav-Drawer">
-                    <div className="Nav-Drawer-Modal">
-                        <div className="Nav-Drawer-Modal-Header">
-                            <button onClick={console.log("Exiting")}>
-                                <i className="material-icons" >
-                                    clear
-                                </i>
-                            </button>                        
-                            <h1>Sunday 22nd July</h1>
-                            <h2>3 Tasks</h2>
-                        </div>
-                        <div className="Nav-Drawer-Modal-Content">
-                            <ul>
-                                <li>
-                                <i className="material-icons">apps</i>
-                                    <p>My Timetable</p>
-                                </li>
-                                <li>
-                                    <i className="material-icons">done</i>
-                                    <p>Finished Tasks</p>
-                                </li>
-                                <li>
-                                    <i className="material-icons">stars</i>
-                                    <p>Achievments</p>
-                                </li>
-                            </ul>
-                        </div>
+        return ( 
+            <div className="Nav-Drawer">
+                <div className="Nav-Drawer-Modal">
+                    <div className="Nav-Drawer-Modal-Header">
+                        <button onClick={this.props.handleClick}>
+                            <i className="material-icons" >
+                                clear
+                            </i>
+                        </button>                        
+                        <h1>Sunday 22nd July</h1>
+                        <h2>3 Tasks</h2>
+                    </div>
+                    <div className="Nav-Drawer-Modal-Content">
+                        <ul>
+                            <li>
+                            <i className="material-icons">apps</i>
+                                <p>My Timetable</p>
+                            </li>
+                            <li>
+                                <i className="material-icons">done</i>
+                                <p>Finished Tasks</p>
+                            </li>
+                            <li>
+                                <i className="material-icons">stars</i>
+                                <p>Achievments</p>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-            );
-        } else {
-            return null
-        }
-        
+            </div>
+        );
     }
 }
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,19 +5,19 @@ import './css/css/index.css';
 class Navbar extends Component {
     constructor(props) {
         super(props);
-        this.state = {isHidden : true}
-        this.toggle = this.toggle.bind(this)
+        this.state = {isHidden: true};
+        this.handleClick = this.handleClick.bind(this);
     }
-    toggle() {
-        this.setState.isHidden = !this.setState.isHidden
+    handleClick() {
+        this.setState({isHidden: !this.state.isHidden})
     }
     render() {
         return (
             <div className="Navbar">
-                <button className="Navbar-Menu-Button" onClick={this.toggle()}>
+                <button className="Navbar-Menu-Button" onClick={this.handleClick}>
                     <i className="material-icons">menu</i> 
                 </button>
-                {this.state.NavDrawerIsHidden ?  null : <NavDrawer />}
+                {this.state.isHidden ? null : <NavDrawer handleClick={this.handleClick} />}
             </div>
         );
     }
