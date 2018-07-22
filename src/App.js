aec1b1ec60b0117e2b80b00921f2fb437d721000
Henry Williams <henrywilliams95000@gmail.com>
2018-07-22 21:30:09 +0100

    Fixed getDate.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar/index';
+import Card from './components/Card/index'
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Navbar />
+                <Card />
             </div>
         );
     }
--- src/components/Nav-Drawer/getDate.js
+++ src/components/Nav-Drawer/getDate.js
@@ -2,6 +2,6 @@ import moment from 'moment';
 
 const date = () => {
     return moment().format('dddd Do MMMM YYYY');
-}
+};
 
 export default date;
