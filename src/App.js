efb30be898ddba4f37ac31aa7d84f63a2229a595
Henry Williams <henrywilliams95000@gmail.com>
2018-08-02 17:50:32 +0100

    Edited styles for nav-drawer, changed how tasks are fetched in card component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import Navbar from './components/Navbar/index';
 import Card from './components/Card/index'
 
+const tasks = [
+    {title : 'Computer Science Exam'},
+    {title : 'Philosophy Exam'},
+    {title : 'Maths Exam'}
+];
+
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Navbar />
-                <Card />
+                <Card tasks={tasks}/>
             </div>
         );
     }
