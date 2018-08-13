9f389b5c0afac92d3ddbdd95d204bb40c25c563e
James-McQueen <james@mcqueen.org.uk>
2018-08-13 21:06:40 +0100

    Repositioned add task button

--- src/components/Add-Task-Button/styles.js
+++ src/components/Add-Task-Button/styles.js
@@ -5,7 +5,9 @@ const AddTaskButtonDiv = styled.div`
         cursor: pointer;
         box-shadow: 0px 5px 5px -3px rgba(0, 0, 0, 0.2), 0px 8px 10px 1px rgba(0, 0, 0, 0.14), 0px 3px 14px 2px rgba(0, 0, 0, 0.12);
         border: none;
-        position: relative;
+        position: absolute;
+        left:    10px;
+        bottom:   10px;
         width: 50px;
         height: 50px;
         border-radius: 50%;
