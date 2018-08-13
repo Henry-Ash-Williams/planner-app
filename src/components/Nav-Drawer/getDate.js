1805892164d0e36573aa4589aa07a90ba86f7156
Henry Williams <henrywilliams95000@gmail.com>
2018-08-13 18:17:27 +0100

    Changed file structure

--- src/components/Nav-Drawer/getDate.js
+++
@@ -1,7 +0,0 @@
-import moment from 'moment';
-
-const date = () => {
-    return moment().format('dddd Do MMMM YYYY');
-};
-
-export default date;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-const removeMargin = { "margin":0, "padding":0, "box-sizing": "border-box" }
-    
-
-
+import registerServiceWorker from './registerServiceWorker'; 
 
 ReactDOM.render(
     <App />,
