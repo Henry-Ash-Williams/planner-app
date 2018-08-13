d0cd9aafb351b73fe6ccf56728b559c3a2a3f8ae
Henry Williams <henrywilliams95000@gmail.com>
2018-08-13 18:35:38 +0100

    Edited formating

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-//const removeMargin = { "margin":0, "padding":0, "box-sizing": "border-box" }
-    
-
-
-import registerServiceWorker from './registerServiceWorker'; 
-
 ReactDOM.render(
     <App />,
     document.getElementById('root')
